7b146ba19e13c9b4c1f398ddbee14bee35730fef
Cem <cemkgrbz@gmail.com>
2022-09-06 14:05:00 +0200

    modified solutions

--- solution.js
+++ solution.js
@@ -1,4 +1,5 @@
 //1
+
 const data = { a: 1 };
 const dataV2 = [5,6,6,8]
 //write your code here 
@@ -12,6 +13,7 @@ console.log(isPlainObject(dataV2))
 console.log(Object.prototype.toString.call(dataV2));
 
 //2
+
 const data2 = { a: 1, b: 2 };
 
 function makePairs(item) {
@@ -40,6 +42,7 @@ console.log(without(data3, 'b')); // { a: 1 }
 
 
 //4
+
 const data4 = { a: 1, b: undefined };
 const data40 = { a:undefined };
 const data22 = {}
@@ -62,7 +65,9 @@ console.log(isEmpty(data40)); // true
 console.log(isEmpty(data22));
 console.log(isEmpty(data33));
 console.log("-------------")
+
 //5
+
 const data5 = { a: 1, b: 1 };  
 const data51 = { a: 1, b: 1 };  
 const data52 = { a: 1, b: 2 };
@@ -98,6 +103,7 @@ console.log(isEqual(data5, data51)); // true
 console.log(isEqual(data5, data52)); // false
 
 //6
+
 const data6 = { a: { b: [1, 2, 3] } }
 //write your code here 
 function invoke(obj, path, func, args) {
@@ -123,6 +129,7 @@ console.log(invoke(data6, 'a.b', 'splice', [1, 2])) // [2, 3]
 
 
 //8
+
 const data8 = { a: 1, b: { c: 1 } };  
 const data81= { a: 1, b: { c: 1 } };  
 const data82= { a: 1, b: { c: 2 } };
@@ -142,8 +149,8 @@ console.log(isEqualDeep(data8, data82)); // false
 console.log("-------------")
 
 
+//9
 
-// //9
 const data9 = { a: 1, b: 2 };  
 const data91 = { c: 1, b: 2 };
 //write your code here 
@@ -164,7 +171,7 @@ console.log(intersection(data9, data91)); // { b: 2 }
 function intersectionV2(object1, object2) {
 
     const keys = Object.keys(object1)
-    console.log("🚀 ~ intersection ~ keys", keys)
+    // console.log("🚀 ~ intersection ~ keys", keys)
 
     const object3 = {}
 
@@ -187,9 +194,35 @@ const idata = { a: 1, b: 2 };
 const idata2 = { c: 1, b: 2 };
 console.log(intersectionV2(idata, idata2));
 
+console.log("-------------")
 
-// //10
-// const data10 = { a: 1, b: { c: 3 } };  
-// const data11 = { c: 1, b: { c: 3 } };
-// //write your code here 
-// console.log(intersectionDeep(data10, data11)); // { b: { c: 3 } }
+
+//10
+
+const data10 = { a: 1, b: { c: 3 } };  
+const data11 = { c: 1, b: { c: 3 } };
+//write your code here 
+
+function intersectionDeep(obj1, obj2, common = {}) {
+
+    const keys1 = Object.keys(obj1)
+
+    for (let key of keys1) {
+
+        if (typeof obj1[key] === "object" && typeof obj2[key] === 'object') {
+
+            intersectionDeep(obj1[key], obj2[key], common)
+
+        } else if(obj1[key] === obj2[key]) {
+            // store the property
+            // console.log('obj1.key=obj2.key')
+
+            common[key] = obj1[key]
+            
+        }
+    }
+
+    return common
+}
+
+console.log(intersectionDeep(data10, data11)); // { b: { c: 3 } }
